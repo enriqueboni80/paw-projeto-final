3077d61b133e7e3cd0ccfb9904b53624f46a9c7b
Enrique Bonifácio Leite / IBSFGV <enrique@ibs.edu.br>
2019-10-17 11:17:53 -0300

    # voltando a classe section, ativando Ajax de redirecionamento das paginas

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    redirecionarPagina("sobre")
+})
+
+
+function redirecionarPagina(pagina) {
+    $.ajax({
+        url: `paginas/${pagina}.html`,
+        data: { "expression": "x" },
+        cache: false
+    }).done(function(retornoRequestPagina) {
+        $("#about-card > .card-content > p").html(retornoRequestPagina);
+    });
+}
